97d34afe0addb3c42da74c77697f4d75f39f7fcd
rkalesalad <rkaelle2@gmail.com>
2025-02-26 19:18:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,9 +30,7 @@ nltk.download('vader_lexicon')
 
 # initialize Firebase
 
-BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ryansdailynews"))
-FIREBASE_CRED_PATH = os.path.join(BASE_DIR, "firebaseCredentials.json")
-cred = credentials.Certificate(FIREBASE_CRED_PATH)
+cred = credentials.Certificate("firebaseCredentials.json")
 firebase_admin.initialize_app(cred, {
     'storageBucket': 'ryansdailynews-7df9d.appspot.com'
 })
